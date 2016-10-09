64a1966e21db22a709c291e70c3544c209f76d93
rodericksolana <you@example.com>
2016-10-09 18:37:44 -0500

    avanzando

--- tareas/tarea4/edgeDetectorCPU.cu
+++ tareas/tarea4/edgeDetectorCPU.cu
@@ -5,25 +5,22 @@
 #include<opencv2/highgui/highgui.hpp>
 #include<cuda_runtime.h>
 
-#define N 3
-#define M 3
-
 using namespace std;
 using namespace cv;
 
+//int *b
 
-
-void sumaEulerCPU1(int *a, int *b, int *c)
+void sumaEulerCPU1(int *a, Mat img, int *c, int N, int M)
 {
-   // Se calculan valores sin considerar la orilla para 3x3.
+   // Se calculan valores sin considerar la orilla para3x3.
    for (int i=1; i<N-1; i++)
       for (int j=1; j<M-1; j++)
-         c[i*M+j] = ( b[i*M+j] * a[4] + b[(i-1)*M+j-1] * a[0] + b[(i-1)*M+j] * a[1] + b[(i-1)*M+j+1] * a[2] + b[i*M+j-1] * a[3] + b[i*M+j+1] * a[5] + b[(i+1)*M+j-1] * a[6] + b[(i+1)*M+j] * a[7] + b[(i+1)*M+j+1] * a[8] ) / 9;
+         c[i*M+j] = ( img.at<uchar>(i*M+j) * a[4] + img.at<uchar>((i-1)*M+j-1) * a[0] + img.at<uchar>((i-1)*M+j) * a[1] + img.at<uchar>((i-1)*M+j+1) * a[2] + img.at<uchar>(i*M+j-1) * a[3] + img.at<uchar>(i*M+j+1) * a[5] + img.at<uchar>((i+1)*M+j-1) * a[6] + img.at<uchar>((i+1)*M+j) * a[7] + img.at<uchar>((i+1)*M+j+1) * a[8] ) / 9;
          // En medio, esq sup izq, arriba, esq sup der, izq, der, esq inf izq, abajo, esq inf der
 }
 
 
-float convolucionCPU1(int *a, int *b, int *c)
+float convolucionCPU1(int *a, Mat img, int *c, int N, int M)
 {
    cudaEvent_t cpuI, cpuF;
    float cpuT;
@@ -31,11 +28,11 @@ float convolucionCPU1(int *a, int *b, int *c)
    cudaEventCreate( &cpuF );
    cudaEventRecord( cpuI, 0 );
 
-   sumaEulerCPU1(a, b, c);
+   sumaEulerCPU1(a, img, c, N, M);
 
    cudaEventRecord( cpuF, 0 );
    cudaEventSynchronize( cpuF );
-   cudaEventElapsedTime( &cpuT, cpuI, cpuF );
+   cudaEventElapsedTime( &cpuT, cpuI, cpuF);
    return cpuT;
 }
 
@@ -68,17 +65,18 @@ int main(int argc, char *argv[])
 	//Allow the windows to resize
 	namedWindow("Input", WINDOW_NORMAL);
 
-	/*int *masPxP, *imaMxN, *resMxN;
-   	masPxP = (int*) malloc(P*P*sizeof(int));
-        imaMxN = (int*) malloc(M*N*sizeof(int));
-        resMxN = (int*) malloc(M*N*sizeof(int));
-	*/
+	int  *resMxN;
+   	//masPxP = (int*) malloc(P*P*sizeof(int));
+        //imaMxN = (int*) malloc(M*N*sizeof(int));
+        resMxN = (int*) malloc(input.rows*input.cols*sizeof(int));
+	
+
+	/*Matrices utilizadas para Prewitt*/
 	int arregloX[9] = {-1,0,1,-1,0,1,-1,0,1};
 
 	int arregloY[9] = {-1,-1,-1,0,0,0,1,1,1};
 
 	int arregloP[9];
-
 
 	int n=3;
 	/*Multiplicación de matrices*/
@@ -103,8 +101,27 @@ int main(int argc, char *argv[])
 	}
 	
 
-	
-	//convolucionCPU1( arregloX, input , arregloP );
+	float tiempo;
+	tiempo= convolucionCPU1( arregloX, input , resMxN, input.rows, input.cols );
+
+	printf("Tiempo %f: ", tiempo);
+
+
+	Mat final;
+	final = input.clone(); //hagamos un clon
+for(int y = 0; y < input.rows; y++) //recorramos las filas
+    for(int x = 0; x < input.cols; x++) //recorramos las columnas
+      final.at<uchar>(y,x) = 0.0; //punto inicial
+
+
+
+
+  for(int y = 1; y < input.rows - 1; y++){
+    for(int x = 1; x < input.cols - 1; x++){
+     
+      final.at<uchar>(y,x) = resMxN[y*x+y]; // y vamos pasando los puntos.
+    }
+  }
 
 
 
@@ -124,7 +141,7 @@ int main(int argc, char *argv[])
 
 
 	//Show the input and output
-	imshow("Input", input);
+	imshow("Input", final);
 
 
 
