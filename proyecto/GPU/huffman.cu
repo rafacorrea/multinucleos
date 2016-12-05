e55e4b72215ece225a5d55d72ee9344eda0a5d82
rafacorrea <rafael_correa_3@hotmail.com>
2016-12-05 09:43:43 -0600

    aaa

--- proyecto/GPU/huffman.cu
+++ proyecto/GPU/huffman.cu
@@ -19,12 +19,13 @@
 #define THREADS_PER_BLOCK 128
 #define WARP_SIZE 32
 
-void add_magic_num(FILE *file);
-void add_bit_vector(FILE *file, unsigned char bit_vector[32]);
-int  add_size(FILE *file, int freq[MAX_CHARS]);
-void add_character_counts(FILE *file, int freq[MAX_CHARS], int num_bytes);
+void add_magic_num(FILE *file); //funcion que anade un numero magico para verificar que es un archivo generado por este proceso
+void add_bit_vector(FILE *file, unsigned char bit_vector[32]); //funcion que anade al archivo la representacion de que caracteres estan presentes (isnerta 256 bits)
+int  add_size(FILE *file, int freq[MAX_CHARS]);//funciona que determina el numero de bits necesarios para representar las frecuencias de los caracteres
+void add_character_counts(FILE *file, int freq[MAX_CHARS], int num_bytes);//funcion que inserta header con las frecuencias de los caractres
 
 
+//funcion para copiar strings en kernels
 __device__ char * my_strcpy(char *dest, const char *src){
   int i = 0;
   while (src[i] != 0)
@@ -35,6 +36,7 @@ __device__ char * my_strcpy(char *dest, const char *src){
   return dest;
 }
 
+//kernel que escribe a un arreglo de chars la representacion en bits de los caracteres scadaos del archivo
 __global__ void encode_byte_stream(char * string, code * code_values, char * res, int * offset, int f_size)
 {
 	int i = blockIdx.x *blockDim.x + threadIdx.x;
@@ -47,6 +49,7 @@ __global__ void encode_byte_stream(char * string, code * code_values, char * res
 	
 }
 
+//kernel que compacta 8 caracteres (0 o 1) en un caracter o byte
 __global__ void compressed_bit_stream(char * encoded_byte_stream, unsigned char * res, int f_size)
 {
 	int i = blockIdx.x *blockDim.x + threadIdx.x;
@@ -67,6 +70,7 @@ __global__ void compressed_bit_stream(char * encoded_byte_stream, unsigned char 
 int main(int argc, char *argv[]) {
 
 
+   //archivos 
    FILE *file_in, *file_out;
    int freq[MAX_CHARS] = {0}, c = 0, count = 0, num_bytes = 0, ret = 0;
    unsigned char bit_vector[32] = {0x00}, packed = 0;
@@ -138,7 +142,7 @@ int main(int argc, char *argv[]) {
    }
 
 
-   //NUEVO **************************************
+   //para tomar tiempo
    
    cudaEvent_t cpuI, cpuF;
    float cpuT;
@@ -147,15 +151,19 @@ int main(int argc, char *argv[]) {
     cudaEventCreate( &cpuF );
     cudaEventRecord( cpuI, 0 );
      
+   //para sacar tamano del archivo
    fseek(file_in, 0, SEEK_END);
    f_size = ftell(file_in);
    fseek(file_in, 0, SEEK_SET);
    
+   //string con el archivo cargado en memoria
    char *string = (char *)malloc(sizeof(char) * f_size); 
+   //arreglo con la longitud de la represnetacion en bits del caracter, sacado de la tabla de huffman
    int * offset = (int *)malloc(sizeof(int) * f_size);
 
    int i = 0;
-
+   
+   //llenar los arreglos
    while ((c = fgetc(file_in)) != EOF) {
       offset[i] = code_values[c].len;
       string[i] = c;
@@ -163,13 +171,17 @@ int main(int argc, char *argv[]) {
       }
    char *d_string;
    
+   //reservar memoria en tarjeta
    cudaMalloc<char>(&d_string, sizeof(char) * f_size);
    cudaMemcpy(d_string, string, f_size*sizeof(char), cudaMemcpyHostToDevice );
    
+   
    thrust::device_vector<int> d_offset(offset, offset+f_size);   
   	
+   //prefix sum en paralelo
    thrust::exclusive_scan(d_offset.begin(), d_offset.end(), d_offset.begin()); // in-place scan
     
+   //copiar a host
    thrust::copy(d_offset.begin(), d_offset.end(), offset);   
    int last = offset[f_size - 1] + code_values[string[f_size-1]].len;
    //padding
@@ -178,6 +190,7 @@ int main(int argc, char *argv[]) {
       last += 8-(last%8);
    }
 
+   //memoria en device
    char * d_encoded_byte_stream;
    char * encoded_byte_stream = (char *)malloc(sizeof(char) * last);
    cudaMalloc<char>(&d_encoded_byte_stream, sizeof(char)*last);
@@ -186,38 +199,45 @@ int main(int argc, char *argv[]) {
    cudaMalloc<code>(&d_code_values, sizeof(code)*MAX_CHARS);   
    cudaMemcpy(d_code_values, code_values, MAX_CHARS*sizeof(code), cudaMemcpyHostToDevice );   
    
+   //sacar pointer de thrust::device_vector
    int * d_offset2 = thrust::raw_pointer_cast( &d_offset[0] );
    //thrust::device_delete(d_offset2);
    int blocks;
    blocks = ceil((float)f_size/THREADS_PER_BLOCK);
 
-printf("blocks: %d\n", blocks);
+   printf("blocks: %d\n", blocks);
    encode_byte_stream<<<blocks,THREADS_PER_BLOCK>>>(d_string, d_code_values, d_encoded_byte_stream, d_offset2, f_size);
    cudaFree(d_string);
    cudaFree(d_code_values);
    //cudaFree(d_offset2);
    
+   //copiar a host
    cudaMemcpy(encoded_byte_stream, d_encoded_byte_stream, last*sizeof(char), cudaMemcpyDeviceToHost);
    
    int finalSize = last/8;
   
+   //memoria en device
    unsigned char * d_encoded_bit_stream;
+   //memoria en host
    unsigned char * encoded_bit_stream = (unsigned char *)malloc(finalSize * sizeof(unsigned char));
    
    cudaMalloc<unsigned char>(&d_encoded_bit_stream, sizeof(unsigned char)*finalSize);
-   blocks = ceil((float)finalSize/THREADS_PER_BLOCK);
+   blocks = ceil((float)finalSize/THREADS_PER_BLOCK); //redondea al siguiente multiplo de 128
    compressed_bit_stream<<<blocks, THREADS_PER_BLOCK>>>(d_encoded_byte_stream, d_encoded_bit_stream, finalSize);
    cudaMemcpy(encoded_bit_stream, d_encoded_bit_stream, finalSize*sizeof(unsigned char), cudaMemcpyDeviceToHost);
    
    
-    
+   //escribe al archivo
    fwrite(encoded_bit_stream, sizeof(unsigned char), finalSize, file_out);
    
+   //parar tiempo
    cudaEventRecord( cpuF, 0 );
    cudaEventSynchronize( cpuF );
    cudaEventElapsedTime( &cpuT, cpuI, cpuF);
     
    printf("Tiempo %f: ", cpuT);
+
+   //FREES
    cudaFree(d_encoded_byte_stream);
    cudaFree(d_encoded_bit_stream);
    
@@ -225,59 +245,7 @@ printf("blocks: %d\n", blocks);
    free(encoded_byte_stream);
    free(string);
    free(offset);
-   /*
-   fseek(file_in, 0, SEEK_SET);
-   
-   //TERMINA NUEVO ************************************
-   
-   
-   
-   // basado en el codigo huffman llenar archivo
-   while ((c = fgetc(file_in)) != EOF) {
-      strncat(temp, code_values[c].path, 2*MAX_PATH);
-      
-      if (strlen(temp) < 8) {
-         continue;
-      }
 
-      // escribir a la salida
-      while (strlen(temp) > 8) {
-         for (count = 0; count < 8; count++) {
-            if (temp[count] == '0') {
-               packed <<= 1;
-            } else {
-               packed = (packed << 1) | 0x01;
-            }
-         }
-
-         fwrite(&packed, sizeof(unsigned char), 1, file_out);
-         packed = 0;
-         strcpy(temp, &temp[8]);
-      }
-   }
-
-   packed = 0;
-
-
-   if (strlen(temp) > 0) {
-      
-      for (count = 0; count < strlen(temp); count++) {
-         if (temp[count] == '0') {
-            packed <<= 1;
-         } else {
-            packed = (packed << 1) | 0x01;
-         }
-      }
-
-      // padding
-      for (count = strlen(temp); count < 8; count++) {
-         packed = (packed << 1) ;
-      }
-
-      // escribir al archivo
-      fwrite(&packed, sizeof(unsigned char), 1, file_out);
-   }
-   */
    // cerrar archivo entrada
    if ((ret = fclose(file_in)) != 0) {
       printf("Failed to close the input file.");
@@ -295,12 +263,12 @@ printf("blocks: %d\n", blocks);
 
 void add_magic_num(FILE *file) {
 
-   unsigned char magic_num[4] = {0x4C,0x70,0xF0,0x7C};
+   unsigned char magic_num[4] = {0x4C,0x70,0xF0,0x7C}; //numero magico
    int i = 0, ret = 0;
 
    for (i = 0; i < 4; i++) {
       if ((ret = fprintf(file, "%c", magic_num[i])) != 1) {
-         printf("Failure to add magic number to output file.\n");
+         printf("Failure to add magic number to output file.\n"); //no se pudo agregar al archivo
          exit(1);
       }
    }
